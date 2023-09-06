dc125baca6b83684d4fe895e5a7050f0ee207d40
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-09-06 11:37:01 -0300

    Criando classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department dp = new Department(1, "Books");
+
+        Seller sl = new Seller(10, "Matheus", "matheus@gmail.com", new Date(), 13000.00, dp);
         System.out.println(dp);
+        System.out.println(sl);
     }
 }
