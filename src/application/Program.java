46728d425a44c7f4541e38c6a874198d13ab468f
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-09-08 10:28:19 -0300

    Implementando SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
     public static void main(String[] args) {
 
         Department dp = new Department(1, "Books");
+
+        SellerDAO sellerDAO = DaoFactory.createSellerDAO();
 
         Seller sl = new Seller(10, "Matheus", "matheus@gmail.com", new Date(), 13000.00, dp);
         System.out.println(dp);
